51be15817f9027f8541703c4a7acca610249751b
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-07-11 08:12:10 +0100

    Removed lib folder and cleared out hash_spec (commented out code and logic).

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -38,8 +38,6 @@ describe 'Create tests for a hash' do
     expect(small_fruit_nutrition_details[:avocado][:calories_from_fat]).to_not be < 20
     expect(small_fruit_nutrition_details[:avocado][:calories_from_fat]).not_to eql 19
     expect(small_fruit_nutrition_details[:avocado][:calories_from_fat]).not_to be_between(0,19)
-    # p small_fruit_nutrition_details[:avocado][:calories_from_fat]
-    # pending
   end
 
   it 'plums and apples should get no calories from fat' do
@@ -48,32 +46,17 @@ describe 'Create tests for a hash' do
 
     expect(small_fruit_nutrition_details[:apple][:calories_from_fat]).to eql 0
     expect(small_fruit_nutrition_details[:apple][:calories_from_fat]).not_to be > 0
-    # pending
   end
 
   it 'each fruit should have 4 keys' do
     expect(small_fruit_nutrition_details[:apple].keys.length).to eql 4
-    # p small_fruit_nutrition_details[:apple].keys.length
-    # pending
   end
 
   it 'all calories should be between 40 to 150' do
-    # expect(small_fruit_nutrition_details[:apple][:calories]).to be_between(40,150)
-    # expect(small_fruit_nutrition_details[:avocado][:calories]).to be_between(40,150)
-    # expect(small_fruit_nutrition_details[:kiwifruit][:calories]).to be_between(40,150)
-    # expect(small_fruit_nutrition_details[:plums][:calories]).to be_between(40,150)
-
-    # p small_fruit_nutrition_details
-
-    small_fruit_nutrition_details.each { |key,value|
-      puts "#{key} is #{value}"
-      fruit_calories = value[:calories]
-      puts "#{key} has #{fruit_calories} calories"
-      if (fruit_calories > 40) && (fruit_calories < 150)
-        puts 'Healthy'
-      end
-    }
-    pending
+    expect(small_fruit_nutrition_details[:apple][:calories]).to be_between(40,150)
+    expect(small_fruit_nutrition_details[:avocado][:calories]).to be_between(40,150)
+    expect(small_fruit_nutrition_details[:kiwifruit][:calories]).to be_between(40,150)
+    expect(small_fruit_nutrition_details[:plums][:calories]).to be_between(40,150)
   end
 
 end
